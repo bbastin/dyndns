fa315b100b659c81ec944240b0c5c13e1365630a
Benedikt Bastin <benedikt@benedikt-bastin.de>
2025-06-06 16:32:38 +0200

    feat: Add config file lookup

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@
 
 #![deny(clippy::all)]
 
-use std::{error::Error, fs::File, io::BufReader};
+use std::{
+    error::Error,
+    fs::File,
+    io::BufReader,
+    path::{Path, PathBuf},
+};
 
 use config::{Config, Providers};
 use providers::hetzner::HetznerProvider;
@@ -13,11 +18,24 @@ mod config;
 pub mod providers;
 mod server;
 
+/// Find first path for the configuration where a file is present
+fn find_config() -> Option<PathBuf> {
+    if Path::new("./config.json").is_file() {
+        return Some(PathBuf::from("./config.json"));
+    }
+
+    if Path::new("/etc/dyndns/config.json").is_file() {
+        return Some(PathBuf::from("/etc/dyndns/config.json"));
+    }
+
+    None
+}
+
 // @TODO this should read in a config file containing multiple users
 // For now, this will not work, but all other functions expect a Vec of users,
 // therefore this will be packed up into an array of exactly one user.
-fn load_config() -> Result<config::User, Box<dyn Error>> {
-    let reader = BufReader::new(File::open("config.json")?);
+fn load_config(path: &Path) -> Result<config::User, Box<dyn Error>> {
+    let reader = BufReader::new(File::open(path)?);
 
     Ok(serde_json::from_reader(reader)?)
 }
@@ -29,7 +47,12 @@ pub async fn main() -> Result<(), rocket::Error> {
         .init()
         .unwrap();
 
-    let config = match load_config() {
+    let path = match find_config() {
+        Some(path) => path,
+        None => panic!("Error finding config: No config found"),
+    };
+
+    let config = match load_config(&path) {
         Ok(user) => Config { users: vec![user] },
         Err(error) => panic!("Error reading config: {error}"),
     };
